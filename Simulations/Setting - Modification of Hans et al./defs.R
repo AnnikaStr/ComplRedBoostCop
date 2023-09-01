833bfeb180fa42b39e6c5cc4c854effd85a9bf54
AnnikaStr <56591849+AnnikaStr@users.noreply.github.com>
2023-09-01 14:00:58 +0200

    Update defs.R

--- Simulations/Setting - Modification of Hans et al./defs.R
+++ Simulations/Setting - Modification of Hans et al./defs.R
@@ -6,11 +6,8 @@ library(stabs)
 
 OVERWRITE = FALSE
 
-
 #settings data generation
-p = c(20)#
-#p = c(200)#
-
+p = c(20)
 #p = c(1000)
 n = c(1000)
 n_test = 1000
@@ -19,14 +16,9 @@ n_train = 1000
 
 # deselection
 TAU_1 = c(0.001,0.01)
-# TAU_1 = c(0.001, 0.01)
-# TAU_plus = c(0.01,0.05,0.1)
 
 # setting stabsel
-PFER = 5
-#PFER = c(1, 2.5)
 Q = c(10, 15, 20)
-
 
 # settings cluster
 MEMORY =  1024L*20L
@@ -34,7 +26,4 @@ NTASKS = 10
 NCPUS = 10
 WALLTIME = 600L*600L
 
-
 REPLICATIONS = 100
-
-
