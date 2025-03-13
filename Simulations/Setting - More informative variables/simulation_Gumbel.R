6967737be67484fbc7716cf0c5b3d91b89a5e5f1
Annika Strömer <56591849+AnnikaStr@users.noreply.github.com>
2025-03-13 14:05:30 +0100

    Update simulation_Gumbel.R

--- Simulations/Setting - More informative variables/simulation_Gumbel.R
+++ Simulations/Setting - More informative variables/simulation_Gumbel.R
@@ -471,7 +471,7 @@ stability <- function(job, data, instance,q){
 
   time = Sys.time()
   mod = glmboostLSS(form, data = train1,
-                    control = boost_control(mstop = 5*nrow(train), nu = 0.01),method = 'noncyclic', families = Gumbel_Cop(marg1 = "NO",
+                    control = boost_control(mstop = 5000, nu = 0.01),method = 'noncyclic', families = Gumbel_Cop(marg1 = "NO",
                                                                                                                           marg2 = "NO"))
   
   if(p == 200) pfer = c(1,2.5,5)
